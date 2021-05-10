f418333c63d607d82d95750d505f59e8e9ccba01
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-05-10 22:20:05 +0200

    start

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
