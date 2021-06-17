44a5582a09b7dffef1da5d890104924274d7c780
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-06-17 18:02:20 +0200

    active and done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,7 +2,14 @@ import React from "react";
 import Task from "./Task";
 
 const TaskList = (props) => {
-  const tasks = props.tasks.map((task) => (
+  const active = props.tasks.filter((task) => task.active);
+  const done = props.tasks.filter((task) => !task.active);
+
+  const activeTasks = active.map((task) => (
+    <Task key={task.id} task={task} delete={props.delete} change={props.change} />
+  ));
+
+  const doneTasks = done.map((task) => (
     <Task key={task.id} task={task} delete={props.delete} change={props.change} />
   ));
 
@@ -10,11 +17,19 @@ const TaskList = (props) => {
     <>
       <div className="active">
         <h1>Zadania do zrobienia</h1>
-        {tasks}
+        {activeTasks.length > 0 ? activeTasks : <p>Brak zadań do wykonania</p>}
       </div>
       <hr />
       <div className="done">
-        <h2>Zadania zrobione (0)</h2>
+        <h2>
+          Zadania zrobione <em>({doneTasks.length})</em>
+        </h2>
+        {done.length > 5 && (
+          <span style={{ color: "red", fontSize: 14 }}>
+            Wyświetlonych jest jedynie 5 ostatnich elementów.
+          </span>
+        )}
+        {doneTasks.slice(0, 5)}
       </div>
     </>
   );
